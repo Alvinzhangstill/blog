5f976ba1948b26b3a27d1aca9195df741148927e
unknown <610013304@qq.com>
2019-11-29 22:14:32 +0800

    '侧边栏跳转'

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import markdown,re
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post,Category
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
 
@@ -40,7 +40,17 @@ def detail(request,pk):
     return render(request,'blog/detail.html',context={'post':post})
 
 
+def archive(request,year,month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
 
+
+def category(request,pk):
+    cate = get_object_or_404(Category,pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
 
 
 
